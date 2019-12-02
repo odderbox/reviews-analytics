6f7cd07ca9dbc407683b7dd079f6a38d6f17d6d1
Derek Chen <odderbox@gmail.com>
2019-12-03 01:36:00 +0800

    add less than 100

--- read.py
+++ read.py
@@ -1,15 +1,20 @@
 data = []
 count = 0
-l = 0
+sum_len = 0
+less_then_h = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		l += len(line)
+		sum_len += len(line)
+		if len(line) < 100:
+			less_then_h.append(line)
 		if count % 10000 == 0:
 			print(len(data))
 			
 
 print('檔案讀取完畢，共有', count, '筆資料')
 
-print('留言平均長度為：', l / count)
+print('留言平均長度為：', sum_len / count)
+
+print('留言小於100長度的筆數有：', len(less_then_h))
